b0f7686796a370a0b35f1b121be59c0122a9d621
JEFERSSON_QE <112734981+Manzanito20003@users.noreply.github.com>
2023-11-13 17:17:12 -0500

    Fronted null

---
+++ P_REACT/zaay/src/Shop.js
@@ -0,0 +1,17 @@
+import Card from "./ComShoop/Card";
+import { Cabeza } from "./componente/Cabeza";
+import Menu from "./componente/Menu";
+
+function Shop() {
+  return (
+        <div>
+            <Cabeza></Cabeza>
+            <Menu></Menu>
+            <Card></Card>
+        </div>
+    );
+}
+
+
+
+export default Shop;
